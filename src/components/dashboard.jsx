56acc4c59029f238a8d1dca4ed5d98aa17e14be8
cjtakhar <cjtakhar@gmail.com>
2023-05-08 12:30:28 -0700

    multiple questions

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -3,28 +3,28 @@ import Response from './response';
 import '../App.css';
 
 const Dashboard = () => {
-    const [question, setQuestion] = useState('');
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      setQuestion(event.target.elements.question.value);
-    };
-  
-    return (
-      <div className="crystalball">
-        <div className="container">
-          <h1 className="title">Welcome.</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="question-container">
-              <input type="text" placeholder="Ask a question" className="question-input" name="question" autocomplete="off" />
-              <button type="submit" className="question-btn">?</button>
-            </div>
-          </form>
-          {question && <Response question={question} />}
-        </div>
-      </div>
-    );
-  };
-  
-  export default Dashboard;
+  const [question, setQuestion] = useState('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuestion(event.target.elements.question.value);
+    event.target.elements.question.value = ''; // clear input field
+  };
+
+  return (
+    <div className="crystalball">
+      <div className="container">
+        <h1 className="title">Welcome.</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="question-container">
+            <input type="text" placeholder="Ask a question" className="question-input" name="question" autoComplete="off" />
+            <button type="submit" className="question-btn">?</button>
+          </div>
+        </form>
+        {question && <Response question={question} />}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/components/response.jsx
+++ src/components/response.jsx
@@ -5,38 +5,44 @@ const Response = ({ question }) => {
   const [response, setResponse] = useState('');
 
   useEffect(() => {
-    const responses = [
-      'It is certain.',
-      'Without a doubt.',
-      'You may rely on it.',
-      'Yes, definitely.',
-      'It is decidedly so.',
-      'As I see it, yes.',
-      'Most likely.',
-      'Yes.',
-      'Outlook good.',
-      'Signs point to yes.',
-      'Reply hazy, try again.',
-      'Better not tell you now.',
-      'Ask again later.',
-      'Cannot predict now.',
-      'Concentrate and ask again.',
-      "Don't count on it.",
-      'Outlook not so good.',
-      'My sources say no.',
-      'Very doubtful.',
-      'My reply is no.'
-    ];
+    if (question) {
+      const responses = [
+        'It is certain.',
+        'Without a doubt.',
+        'You may rely on it.',
+        'Yes, definitely.',
+        'It is decidedly so.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Yes.',
+        'Outlook good.',
+        'Signs point to yes.',
+        'Reply hazy, try again.',
+        'Better not tell you now.',
+        'Ask again later.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        "Don't count on it.",
+        'Outlook not so good.',
+        'My sources say no.',
+        'Very doubtful.',
+        'My reply is no.'
+      ];
 
-    const randomIndex = Math.floor(Math.random() * responses.length);
-    setResponse(responses[randomIndex]);
+      const randomIndex = Math.floor(Math.random() * responses.length);
+      setResponse(responses[randomIndex]);
+    } else {
+      setResponse(''); // clear response if question is empty
+    }
   }, [question]);
 
   return (
     <div className="answer-container">
-      <TypeText text={response} fontSize="2rem" delay={100} />
+      {response && <TypeText text={response} fontSize="2rem" delay={100} />}
     </div>
   );
 };
 
 export default Response;
+
+
